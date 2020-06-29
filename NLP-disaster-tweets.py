87fd9cc1a729393be2b2be5087e17704a61384ff
Kavish Goyal <48274723+kvtheckrock@users.noreply.github.com>
2020-06-29 11:26:55 +0530

    Add files via upload

---
+++ NLP-disaster-tweets.py
@@ -0,0 +1,273 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import sklearn as skl
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import feature_extraction, linear_model, model_selection, preprocessing
+import string 
+
+from wordcloud import STOPWORDS
+
+
+# In[14]:
+
+
+
+
+
+# In[4]:
+
+
+data = pd.read_csv('train.csv')
+train_df = pd.DataFrame(data)
+test = pd.read_csv('test.csv')
+test_df = pd.DataFrame(test)
+
+
+# In[5]:
+
+
+train_df.head(10)
+
+
+# In[6]:
+
+
+train_df.isnull().sum()
+
+
+# In[7]:
+
+
+train_df.describe()
+
+
+# In[8]:
+
+
+train_df[train_df["target"] == 0]["text"].values[1]
+
+
+# In[9]:
+
+
+train_df[train_df["target"] == 1]["text"].values[2]
+
+
+# In[10]:
+
+
+train_df.loc[train_df['text'].str.contains('fire', na=False, case=False)]
+
+
+# In[11]:
+
+
+# word_count
+train_df['word_count'] = train_df['text'].apply(lambda x: len(str(x).split()))
+test_df['word_count'] = test_df['text'].apply(lambda x: len(str(x).split()))
+
+# unique_word_count
+train_df['unique_word_count'] = train_df['text'].apply(lambda x: len(set(str(x).split())))
+test_df['unique_word_count'] = test_df['text'].apply(lambda x: len(set(str(x).split())))
+
+# stop_word_count
+train_df['stop_word_count'] = train_df['text'].apply(lambda x: len([w for w in str(x).lower().split() if w in STOPWORDS]))
+test_df['stop_word_count'] = test_df['text'].apply(lambda x: len([w for w in str(x).lower().split() if w in STOPWORDS]))
+
+# url_count
+train_df['url_count'] = train_df['text'].apply(lambda x: len([w for w in str(x).lower().split() if 'http' in w or 'https' in w]))
+test_df['url_count'] = test_df['text'].apply(lambda x: len([w for w in str(x).lower().split() if 'http' in w or 'https' in w]))
+
+# mean_word_length
+train_df['mean_word_length'] = train_df['text'].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
+test_df['mean_word_length'] = test_df['text'].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
+
+# char_count
+train_df['char_count'] = train_df['text'].apply(lambda x: len(str(x)))
+test_df['char_count'] = test_df['text'].apply(lambda x: len(str(x)))
+
+# punctuation_count
+train_df['punctuation_count'] = train_df['text'].apply(lambda x: len([c for c in str(x) if c in string.punctuation]))
+test_df['punctuation_count'] = test_df['text'].apply(lambda x: len([c for c in str(x) if c in string.punctuation]))
+
+# hashtag_count
+train_df['hashtag_count'] = train_df['text'].apply(lambda x: len([c for c in str(x) if c == '#']))
+test_df['hashtag_count'] = test_df['text'].apply(lambda x: len([c for c in str(x) if c == '#']))
+
+# mention_count
+train_df['mention_count'] = train_df['text'].apply(lambda x: len([c for c in str(x) if c == '@']))
+test_df['mention_count'] = test_df['text'].apply(lambda x: len([c for c in str(x) if c == '@']))
+
+
+# In[12]:
+
+
+train_df.head()
+
+
+# In[32]:
+
+
+test_df.head()
+
+
+# In[15]:
+
+
+missing_cols = ['keyword', 'location']
+
+fig, axes = plt.subplots(ncols=2, figsize=(17, 4), dpi=100)
+
+sns.barplot(x=train_df[missing_cols].isnull().sum().index, y=train_df[missing_cols].isnull().sum().values, ax=axes[0])
+sns.barplot(x=test_df[missing_cols].isnull().sum().index, y=test_df[missing_cols].isnull().sum().values, ax=axes[1])
+
+axes[0].set_ylabel('Missing Value Count', size=15, labelpad=20)
+axes[0].tick_params(axis='x', labelsize=15)
+axes[0].tick_params(axis='y', labelsize=15)
+axes[1].tick_params(axis='x', labelsize=15)
+axes[1].tick_params(axis='y', labelsize=15)
+
+axes[0].set_title('Training Set', fontsize=13)
+axes[1].set_title('Test Set', fontsize=13)
+
+plt.show()
+
+for df in [train_df, test_df]:
+    for col in ['keyword', 'location']:
+        df[col] = df[col].fillna(f'no_{col}')
+
+
+# In[16]:
+
+
+print(f'Number of unique values in keyword = {train_df["keyword"].nunique()} (Training) - {test_df["keyword"].nunique()} (Test)')
+print(f'Number of unique values in location = {train_df["location"].nunique()} (Training) - {test_df["location"].nunique()} (Test)')
+
+
+# In[19]:
+
+
+train_df['target_mean'] = train_df.groupby('keyword')['target'].transform('mean')
+
+fig = plt.figure(figsize=(8, 72), dpi=100)
+
+sns.countplot(y=train_df.sort_values(by='target_mean', ascending=False)['keyword'],
+              hue=train_df.sort_values(by='target_mean', ascending=False)['target'])
+
+plt.tick_params(axis='x', labelsize=15)
+plt.tick_params(axis='y', labelsize=12)
+plt.legend(loc=1)
+plt.title('Target Distribution in Keywords')
+
+plt.show()
+
+train_df.drop(columns=['target_mean'], inplace=True)
+
+
+# In[21]:
+
+
+METAFEATURES = ['word_count', 'unique_word_count', 'stop_word_count', 'url_count', 'mean_word_length',
+                'char_count', 'punctuation_count', 'hashtag_count', 'mention_count']
+DISASTER_TWEETS = train_df['target'] == 1
+
+fig, axes = plt.subplots(ncols=2, nrows=len(METAFEATURES), figsize=(20, 50), dpi=100)
+
+for i, feature in enumerate(METAFEATURES):
+    sns.distplot(train_df.loc[~DISASTER_TWEETS][feature], label='Not Disaster', ax=axes[i][0], color='green')
+    sns.distplot(train_df.loc[DISASTER_TWEETS][feature], label='Disaster', ax=axes[i][0], color='red')
+
+    sns.distplot(train_df[feature], label='Training', ax=axes[i][1])
+    sns.distplot(test_df[feature], label='Test', ax=axes[i][1])
+    
+    for j in range(2):
+        axes[i][j].set_xlabel('')
+        axes[i][j].tick_params(axis='x', labelsize=12)
+        axes[i][j].tick_params(axis='y', labelsize=12)
+        axes[i][j].legend()
+    
+    axes[i][0].set_title(f'{feature} Target Distribution in Training Set', fontsize=13)
+    axes[i][1].set_title(f'{feature} Training & Test Set Distribution', fontsize=13)
+
+plt.show()
+
+
+# In[22]:
+
+
+fig, axes = plt.subplots(ncols=2, figsize=(17, 4), dpi=100)
+plt.tight_layout()
+
+train_df.groupby('target').count()['id'].plot(kind='pie', ax=axes[0], labels=['Not Disaster (57%)', 'Disaster (43%)'])
+sns.countplot(x=train_df['target'], hue=train_df['target'], ax=axes[1])
+
+axes[0].set_ylabel('')
+axes[1].set_ylabel('')
+axes[1].set_xticklabels(['Not Disaster (4342)', 'Disaster (3271)'])
+axes[0].tick_params(axis='x', labelsize=15)
+axes[0].tick_params(axis='y', labelsize=15)
+axes[1].tick_params(axis='x', labelsize=15)
+axes[1].tick_params(axis='y', labelsize=15)
+
+axes[0].set_title('Target Distribution in Training Set', fontsize=13)
+axes[1].set_title('Target Count in Training Set', fontsize=13)
+
+plt.show()
+
+
+# In[35]:
+
+
+ORIGINAL_FEATURES = ['id', 'keyword', 'location', 'text', 'target']
+
+train_df[ORIGINAL_FEATURES].to_pickle('train.pkl')
+test_df[ORIGINAL_FEATURES].to_pickle('test.pkl')
+
+submission = pd.read_csv("sample_submission.csv")
+submission['target'] = test_df['target'].values
+submission.to_csv('submission.csv', index=False)
+
+print('Training Set Shape = {}'.format(train_df[ORIGINAL_FEATURES].shape))
+print('Training Set Memory Usage = {:.2f} MB'.format(train_df[ORIGINAL_FEATURES].memory_usage().sum() / 1024**2))
+print('Test Set Shape = {}'.format(test_df[ORIGINAL_FEATURES].shape))
+print('Test Set Memory Usage = {:.2f} MB'.format(test_df[ORIGINAL_FEATURES].memory_usage().sum() / 1024**2))
+
+
+# In[37]:
+
+
+submission.head(20)
+
+
+# In[ ]:
+
+
+count_vectorizer = feature_extraction.text.CountVectorizer()
+pickle.dump(count_vectorizer, open('tranform.pkl', 'wb'))
+## let's get counts for the first 5 tweets in the data
+example_train_vectors = count_vectorizer.fit_transform(train_df["text"][0:5])
+## we use .todense() here because these vectors are "sparse" (only non-zero elements are kept to save space)
+print(example_train_vectors[0].todense().shape)
+print(example_train_vectors[0].todense())
+## Our vectors are really big, so we want to push our model's weights
+## toward 0 without completely discounting different words - ridge regression 
+## is a good way to do this.
+clf = linear_model.RidgeClassifier()
+scores = model_selection.cross_val_score(clf, train_vectors, train_df["target"], cv=3, scoring="f1")
+scores
+clf.fit(train_vectors, train_df["target"])## Our vectors are really big, so we want to push our model's weights
+## toward 0 without completely discounting different words - ridge regression 
+## is a good way to do this.
+clf = linear_model.RidgeClassifier()
+scores = model_selection.cross_val_score(clf, train_vectors, train_df["target"], cv=3, scoring="f1")
+scores
+clf.fit(train_vectors, train_df["target"])
+filename = 'nlp_model.pkl'
+pickle.dump(clf, open(filename, 'wb'))
---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
+
+
+# load the model from disk
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv=pickle.load(open('tranform.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+    return render_template('result.html',prediction = my_prediction)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
